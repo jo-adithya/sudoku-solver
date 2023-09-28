96e9766d3faa3dfbe3413c8c596426215bf733ac
Jonathan Adithya <jo.adithya391@gmail.com>
2023-09-28 16:43:46 -0700

    add click outside board fucntionality

--- app/src/components/Board.tsx
+++ app/src/components/Board.tsx
@@ -1,16 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
+import { useOnClickOutside } from 'usehooks-ts';
 import { BoardContext } from '../context/board-context';
 import { Box } from './Box';
 
 import './Board.css';
 
 export const Board = () => {
+  const ref = useRef<HTMLDivElement>(null);
   const boardCtx = useContext(BoardContext);
-
   const size = boardCtx.size;
 
+  useOnClickOutside(ref, () => {
+    boardCtx.setActiveUnit(null);
+  });
+
   return (
-    <div className="board flex flex-col gap-2">
+    <div ref={ref} className="board flex flex-col gap-2">
       {[...Array(size)].map((_, i) => (
         <div key={i} className="flex gap-2">
           {[...Array(size)].map((_, j) => (
